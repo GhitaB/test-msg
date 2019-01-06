28f524e1841da3c492d8c1534be640d3a3268712
Ghiță Bizău <ghita_bizau@yahoo.com>
2019-01-06 13:31:53 +0200

    Simple working example.

---
+++ script.js
@@ -0,0 +1,11 @@
+function getUrlVars() {
+  var vars = {};
+  var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi,
+  function(m,key,value) {
+    vars[key] = value;
+  });
+  return vars;
+}
+
+var name = getUrlVars()["name"];
+document.getElementById("name-here").innerHTML = name;
